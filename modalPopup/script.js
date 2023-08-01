79fd1966346f0b2b063974503779dd464c3955cc
karan bhilware <karan.bhilware@milkbasket.com>
2023-08-01 16:56:05 +0530

    modal

---
+++ modalPopup/script.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const modal = document.querySelector('.modal'),
+    overlay = document.querySelector('.overlay'),
+    btnCloseModal = document.querySelector('.close-modal'),
+    btnsOpenModal = document.querySelectorAll('.show-modal');
+    const closeModal = () => {
+        overlay.classList.add('hidden');
+        modal.classList.add('hidden');
+    }
+    const openModalHandler = () =>{
+        modal.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+    }
+    
+    for(let i = 0; i < btnsOpenModal.length; i++){
+        btnsOpenModal[i].addEventListener('click',openModalHandler)
+    }
+    
+    btnCloseModal.addEventListener('click',closeModal);
+    overlay.addEventListener('click',closeModal);
+    
+    document.addEventListener('keydown',function(e){
+       if(!modal.classList.contains('hidden') && e.key === "Escape")closeModal();
+    }); 
